7166aa4620312c42f10d8c3d16d1aadd5f937e30
David Nesca <Dnesca@rrc.ca>
2025-11-09 23:24:43 -0600

    fixed errors and built scaffolfding for styles and script files

---
+++ script.js
@@ -0,0 +1,6 @@
+// This waits for the page to finish loading before running setup code
+window.onload = function () {
+  // This writes a message to the console to confirm JS is connected
+  console.log("Assignment 4 setup loaded.");
+  // This ends the onload handler
+};
